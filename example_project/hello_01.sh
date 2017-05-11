be30fc1b51661bd1be790f93e7c3777a42ab409f
otiai10 <otiai10@gmail.com>
2017-05-11 10:59:03 +0900

    change example a bit (to specify tail behavior)

--- example_project/hello_01.sh
+++ example_project/hello_01.sh
@@ -2,8 +2,13 @@
 
 #$ -S /bin/sh
 
-echo "This is hello_01!!"
-date
-sleep 20
-date
-echo "END: hello_01"
+echo "This is hello_01!! `date`"
+i=0
+while [ ${i} -lt 5 ]
+do
+	sleep 1
+	printf "."
+	i=$(( i + 1 ))
+done
+echo ""
+echo "END: hello_01 `date`"
